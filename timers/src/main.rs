cc6f9ca59e6dac324fad8afead4f90bba91058d8
lianro2000-prog <lianro2000@gmail.com>
2025-12-24 15:38:20 +0000

    Set the timer registers as part of the exercise.

--- timers/src/main.rs
+++ timers/src/main.rs
@@ -14,12 +14,32 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// Refer to the board documentation
+// https://www.ti.com/lit/ds/symlink/lm3s6965.pdf
+// Table 5-5
+
+// Run-Mode Clock Configuration (RCC)
+const RCC: u32 = 0x400FE060;
+// Prescaler value
+const SYSCTL_SYSDIV_16: u32 = 0xF;
+const SYSCTL_SYSDIV_12: u32 = 0xB;
+
 // CPU frequency (12.5 MHz by default)
 const CPU_FREQ: u32 = 12_500_000;
 
 #[entry]
 fn main() -> ! {
     hprintln!("Starting program!");
+
+    // Set the prescaler value
+    unsafe {
+        // let sysdiv: u32 = SYSCTL_SYSDIV_16 << 23; // shift to bits 23-26
+        let sysdiv: u32 = SYSCTL_SYSDIV_12 << 23; // This is faster than _16.
+        let orig: u32 = *(RCC as *const u32);
+        let mask: u32 = !0b1111 << 23;
+        let rcc: u32 = (orig & mask) | sysdiv;
+        *(RCC as *mut u32) = rcc;
+    }
 
     let peripherals = Peripherals::take().unwrap();
     let mut systick = peripherals.SYST;
